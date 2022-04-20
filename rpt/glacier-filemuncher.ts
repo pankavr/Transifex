442fc4c42fc8540a24bd0cab781ed592f18e2a03
pankav <55692244+pankav@users.noreply.github.com>
2022-04-20 12:01:16 +0300

    Add files via upload

---
+++ rpt/glacier-filemuncher.ts
@@ -0,0 +1,134 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>DetailViewSheet</name>
+    <message>
+        <location filename="../src/qml/DetailViewSheet.qml" line="53"/>
+        <location filename="../src/qml/DetailViewSheet.qml" line="225"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DetailViewSheet.qml" line="59"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DetailViewSheet.qml" line="84"/>
+        <source>Name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DetailViewSheet.qml" line="93"/>
+        <source>Enter a new name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DetailViewSheet.qml" line="119"/>
+        <source>Path:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DetailViewSheet.qml" line="147"/>
+        <source>Size:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DetailViewSheet.qml" line="174"/>
+        <source>Created:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DetailViewSheet.qml" line="201"/>
+        <source>Last Modified:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DetailViewSheet.qml" line="239"/>
+        <source>Ok</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DirectoryPage</name>
+    <message>
+        <location filename="../src/qml/DirectoryPage.qml" line="54"/>
+        <source>File manager</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DirectoryPage.qml" line="69"/>
+        <source>Create directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DirectoryPage.qml" line="74"/>
+        <source>Enter new folder name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DirectoryPage.qml" line="86"/>
+        <source>Hide hidden files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DirectoryPage.qml" line="86"/>
+        <source>Show hidden files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DirectoryPage.qml" line="182"/>
+        <source>No files here</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DirectoryPage.qml" line="234"/>
+        <source>Open file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DirectoryPage.qml" line="236"/>
+        <location filename="../src/qml/DirectoryPage.qml" line="248"/>
+        <source>Yes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DirectoryPage.qml" line="237"/>
+        <location filename="../src/qml/DirectoryPage.qml" line="249"/>
+        <source>No</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DirectoryPage.qml" line="247"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DirectoryPage.qml" line="258"/>
+        <source>Ok</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>InputSheet</name>
+    <message>
+        <location filename="../src/qml/InputSheet.qml" line="70"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/InputSheet.qml" line="84"/>
+        <source>Ok</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/main.cpp" line="62"/>
+        <source>Files</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
